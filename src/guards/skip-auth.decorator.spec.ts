b775465603ad5c3070116ac2bff1bb0f23f57075
Vlad Lan <vladislav_lanskikh@epam.com>
2022-10-31 11:32:41 +0100

    Ran formatting

--- src/guards/skip-auth.decorator.spec.ts
+++ src/guards/skip-auth.decorator.spec.ts
@@ -1,8 +1,8 @@
-import { SkipAuth } from "./skip-auth.decorator";
+import { SkipAuth } from './skip-auth.decorator';
 
-describe("SkipAuth", () => {
-    it("Should return SetMetadata fn", () => {
-        const res = SkipAuth();
-        expect(res.KEY).toBe("skip-auth")
-    })
-})
+describe('SkipAuth', () => {
+  it('Should return SetMetadata fn', () => {
+    const res = SkipAuth();
+    expect(res.KEY).toBe('skip-auth');
+  });
+});
